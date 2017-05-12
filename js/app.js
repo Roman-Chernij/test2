f791f0da4fe5ffb54ea4eef1b5c2cc836fff0bcf
Roman <roman.cherniy89@gmail.com>
2017-05-12 14:35:07 +0300

    bug fixes two

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
+
+
 ;(function() {
     $(document).foundation();
 })();
+
+window.addEventListener('scroll' ,function () {
+    new Paralax();
+    new FixedToBar();
+
+});
 
 
 window.addEventListener( "DOMContentLoaded", function() {
@@ -12,16 +20,16 @@ function MyScript() {
   new MyBurger();
   new ClickScrol();
   new ChangeWindow();
+
 }
 
 function MyBurger() {
-    //alert(' защел в MyBurger');
     this.burger = document.querySelector('.burger');
     this.burger.addEventListener('click', this.burgerAddEvent.bind(this));
 }
 
 MyBurger.prototype.burgerAddEvent = function () {
-    //alert(' защел в burgerAddEvent');
+    new Ww();
     this.burgerChild = this.burger.querySelector('.burger__item');
     this.burgerChild.classList.toggle("burger_active");
 };
@@ -41,8 +49,8 @@ ClickScrol.prototype.addEvntScrol = function () {
 ClickScrol.prototype.scrolEvent = function () {
     var top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
     if(top > 0) {
-        window.scrollBy(0,-50);
-        this.t = setTimeout(this.scrolEvent.bind(this), 20);
+        window.scrollBy(0,-15);
+        this.t = setTimeout(this.scrolEvent.bind(this), 10);
     } else clearTimeout(this.t);
     return false;
 };
@@ -60,13 +68,9 @@ function Ww() {
 
 
 Ww.prototype.widthChange = function () {
-    this.head = document.querySelector('.header');
-    this.headHeight = this.head.clientHeight;
 
     if(this.widthWindow < '1020') {
         innerNav();
-        var mobi = document.querySelector('#offCanvasRight > .mobi-menu');
-        mobi.style.cssText +="height: "+this.headHeight+"px;"
     } else if(this.widthWindow > '1020') {
         removeChild();
     }
@@ -82,9 +86,11 @@ function innerNav() {
     if(!check){
         var nav = document.querySelector('.nav__nav-list'),
             cloneNav = nav.cloneNode(true),
-            r = cloneNav,
+            phone = document.querySelector('.phone'),
+            clonePhone = phone.cloneNode(true),
             t = document.querySelector('#offCanvasRight');
         t.insertAdjacentHTML('afterBegin', str);
+        t.appendChild(clonePhone);
         t.appendChild(cloneNav);
     }
         return
@@ -99,7 +105,69 @@ function removeChild() {
 }
 
 function patern() {
-    return  '<div class="mobi-menu">'+
-        '<h2 class="mobi-menu__title">Menu</h2>'+
-        '</div>'
+    return  '<div class="mobi-menu"></div>'
+};
+
+
+
+
+function Paralax() {
+    var scrolled = window.pageYOffset || document.documentElement.scrollTop,
+        banner = document.querySelector('.banner__paralax'),
+        bannerAttr = banner.getAttribute('data-speed'),
+        numberBanner = scrolled/bannerAttr;
+    banner.style.cssText +="transform: translateY("+numberBanner+"px);";
+}
+
+function FixedToBar() {
+    this.header = document.querySelector('.header');
+    this.scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    this.headerHegth = parseInt(getComputedStyle(this.header).height);
+    this.fixedTopBar = document.querySelector('.fixed-top-bar');
+
+
+
+    if(!this.fixedTopBar) {
+        this.fixedAddElem();
+    }else {this.removeFixedMenu();}
+
+
+}
+
+FixedToBar.prototype.fixedAddElem = function () {
+    this.topBar = this.header.querySelector('.top-bar');
+    this.cloneTopBar = this.topBar.cloneNode(true);
+    this.offCanvasContent = document.querySelector('.off-canvas-content');
+    this.row = document.querySelector('.row');
+    this.rowWidht = getComputedStyle(this.row).width;
+    this.markur = '<div class="fixed-top-bar"></div>';
+
+    if(this.scrolled > this.headerHegth){
+        this.addFixedMenu();
+    }
+
+};
+
+FixedToBar.prototype.addFixedMenu = function () {
+    this.offCanvasContent.insertAdjacentHTML('afterBegin', this.markur);
+    this.fixedTopBarAdd = document.querySelector('.fixed-top-bar');
+    this.fixedTopBarAdd.appendChild(this.cloneTopBar);
+    this.fixedTopBarAdd.style.cssText +="width:"+this.rowWidht+";";
+};
+
+FixedToBar.prototype.removeFixedMenu = function () {
+    if(this.scrolled < this.headerHegth) {
+        this.parentFixed = document.querySelector('.off-canvas-content');
+        this.fix = this.parentFixed.querySelector('.fixed-top-bar');
+            if(this.fix == 'null'){
+                return
+                }else if(this.fix != 'null') {
+                this.fix.classList.add('fixed-top-bar_hide');
+                this.fix.addEventListener('animationend', this.removeFixed.bind(this))
+            }
+    }
+};
+
+FixedToBar.prototype.removeFixed = function () {
+    this.parentFixed.removeChild(this.fix);
 };
